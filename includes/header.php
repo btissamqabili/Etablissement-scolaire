d92622281ac4ca77df61e8744fd4d99d4e180af6
btissamqabili <qabilibtissam8@gmail.com>
2026-06-18 16:15:17 +0100

    ajout du PDO

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion scolaire</title>
+</head>
+<body>
---
+++ includes/navbar.php
@@ -0,0 +1,9 @@
+<a href="/index.php">Accueil</a>
+<a href="/classes/liste.php">Classes</a>
+<a href="/matieres/liste.php">Matières</a>
+<a href="/eleves/liste.php">Élèves</a>
+<a href="/enseignants/liste.php">Enseignants</a>
+<a href="/inscriptions/liste.php">Inscriptions</a>
+<a href="/affectations/liste.php">Affectations</a>
+
+<hr>
